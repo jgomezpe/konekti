1eb868d5c4fdd3e94e03471a7c4731275b5955ef
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-09-06 18:27:05 -0500

    Improving components setting up

--- src/header.js
+++ src/header.js
@@ -4,6 +4,25 @@ class HeaderPlugIn extends PlugIn{
 	constructor(){ super('header') }
     
 	/**
+	 * Creates a Header configuration object
+	 * @method
+	 * headerConfig
+	 * @param id Id of the header/Configuration of the header
+	 * @param icon Icon for the header
+	 * @param caption Caption of the header
+	 * @param h Size of the header (1,2,3..)
+	 * @param style Style of the header
+	 * @param parent Parent component
+	 */
+	setup(id, icon, caption, h, style, parent='KonektiMain'){
+		var config = id
+		if(typeof id == 'string') 
+			config = {'id':id, 'style':style, 'h':h, 'parent':parent, 'children':[Konekti.plugins['item'].setup(id+'Item', icon, caption, id)]}
+		config.plugin = 'item'
+		return config
+	}
+
+	/**
 	 * Creates a client for the plugin's instance
 	 * @param config Instance configuration
 	 */
@@ -29,21 +48,6 @@ class Header extends Client{
 if(Konekti.header===undefined) new HeaderPlugIn()
 
 /**
- * Creates a Header configuration object
- * @method
- * headerConfig
- * @param id Id of the header/Configuration of the header
- * @param icon Icon for the header
- * @param caption Caption of the header
- * @param h Size of the header (1,2,3..)
- * @param style Style of the header
- * @param parent Parent component
- */
-Konekti.headerConfig = function(id, icon, caption, h, style, parent='KonektiMain'){
-	return {'plugin':'header', 'id':id, 'style':style, 'h':h, 'parent':parent, 'children':[Konekti.itemConfig(id+'Item', icon, caption, id)]}
-}
-
-/**
  * Associates/adds a header
  * @method
  * header
@@ -54,5 +58,5 @@ Konekti.headerConfig = function(id, icon, caption, h, style, parent='KonektiMain
  * @param style Style of the header
  */
 Konekti.header = function(id, icon, caption, h, style){
-	return Konekti.build(Konekti.headerConfig(id, icon, caption, h, style))
+	return Konekti.build(Konekti.plugin['header'].setup(id, icon, caption, h, style))
 }
