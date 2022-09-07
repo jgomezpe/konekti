8e61d7f79f5e6fc586c3549baf8b762d5cb6f94f
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-09-06 22:46:53 -0500

    Using setup method

--- src/accordion.js
+++ src/accordion.js
@@ -4,7 +4,36 @@ uses('header')
 class AccordionPlugIn extends PlugIn{
 	/** Creates a Plugin for Accordion components */
 	constructor(){ super('accordion') }
-    
+
+	/**
+	 * Creates an accordion configuration object
+	 * @method
+	 * accordionConfig
+	 * @param id Id of the header
+	 * @param width Width of the sidebar
+	 * @param height Height of the div's component
+	 * @param icon Icon for the header
+	 * @param caption Caption of the header
+	 * @param h Size of the header (1,2,3..)
+	 * @param style Style of the header
+	 * @param content Content component
+	 * @param open If content component should be displayed or not
+	 * @param parent Parent component
+	 */
+	setup(id, icon, caption, h, style, content, open, parent='KonektiMain'){
+		var config = id
+		if(typeof id == 'string'){
+			var children = [Konekti.plugin['header'].setup(id+'Item', icon, caption, h, style, id)]
+			if(content!=null){
+				content.parent = content.parent || id
+				content.id = content.id || id+'Content'
+				children.push(content)
+			}
+			config ={'id':id, 'parent':parent, 'open':open, 'children':children}
+		}
+		config.plugin = 'accordion'
+		return config
+	}	
 	/**
 	 * Creates a client for the plugin's instance
 	 * @param thing Instance configuration
@@ -67,31 +96,6 @@ class Accordion extends Client{
 if(Konekti.accordion === undefined) new AccordionPlugIn()
 
 /**
- * Creates an accordion configuration object
- * @method
- * accordionConfig
- * @param id Id of the header
- * @param width Width of the sidebar
- * @param height Height of the div's component
- * @param icon Icon for the header
- * @param caption Caption of the header
- * @param h Size of the header (1,2,3..)
- * @param style Style of the header
- * @param content Content component
- * @param open If content component should be displayed or not
- * @param parent Parent component
- */
-Konekti.accordionConfig = function(id, icon, caption, h, style, content, open, parent='KonektiMain'){
-	var children = [Konekti.headerConfig(id+'Item', icon, caption, h, style, id)]
-	if(content!=null){
-		content.parent = content.parent || id
-		content.id = content.id || id+'Content'
-		children.push(content)
-	}
-	return {'plugin':'accordion', 'id':id, 'parent':parent, 'open':open, 'children':children}
-}
-
-/**
  * Associates/adds an accordion
  * @method
  * accordion
@@ -107,32 +111,41 @@ Konekti.accordion = function(id, icon, caption, h, style, content, open){
 	return Konekti.build(Konekti.accordionConfig(id, icon, caption, h, style, content, open))
 }
 
-/**
- * Creates a TOC configuration object
- * @method
- * tocConfig
- * @param tree Table of Content component 
- * @param h Size of the main content (1,2,3..) recommended 3 or 4, maximum 6
- * @param style Style of the toc
- * @param onclick Method called when an item is selected
- * @param open If toc component should be displayed or not
- * @param parent Parent component
- */
-Konekti.tocConfig = function(tree, h, style, onclick, open, parent='KonektiMain'){
-	h = Math.min(h,6)
-	var content =null
-	if(tree.children !== undefined){
-		content = Konekti.divConfig(tree.id+'Content', '', '', '', '', tree.id)		
-		content.children = []
-		for( var i=0; i<tree.children.length; i++ ){
-			content.children.push(Konekti.tocConfig(tree.children[i], h+1, style, onclick, false, tree.id+'Content'))
+/** Konekti Plugin for TOC (Table of content) components */
+class TocPlugIn extends PlugIn{
+	/** Creates a Plugin for TOC components */
+	constructor(){ super('toc') }
+
+	/**
+	 * Creates a TOC configuration object
+	 * @method
+	 * tocConfig
+	 * @param tree Table of Content component 
+	 * @param h Size of the main content (1,2,3..) recommended 3 or 4, maximum 6
+	 * @param style Style of the toc
+	 * @param onclick Method called when an item is selected
+	 * @param open If toc component should be displayed or not
+	 * @param parent Parent component
+	 */
+	setup(tree, h, style, onclick, open, parent='KonektiMain'){
+		h = Math.min(h,6)
+		var content =null
+		if(tree.children !== undefined){
+			content = Konekti.divConfig(tree.id+'Content', '', '', '', '', tree.id)		
+			content.children = []
+			for( var i=0; i<tree.children.length; i++ ){
+				content.children.push(Konekti.tocConfig(tree.children[i], h+1, style, onclick, false, tree.id+'Content'))
+			}
 		}
+		var item = super.setup(tree.id, tree.icon, tree.caption, h, style, content, open, parent)
+		if(tree.action === undefined) tree.action = true
+		item.expand = tree.action?onclick:function(id){}
+		return item
 	}
-	var item = Konekti.accordionConfig(tree.id, tree.icon, tree.caption, h, style, content, open, parent)
-	if(tree.action === undefined) tree.action = true
-	item.expand = tree.action?onclick:function(id){}
-	return item
 }
+
+/** Accordion class */
+if(Konekti.toc === undefined) new TocPlugIn()
 
 /**
  * Associates/adds a table of contents
@@ -145,5 +158,5 @@ Konekti.tocConfig = function(tree, h, style, onclick, open, parent='KonektiMain'
  * @param open If toc component should be displayed or not
  */
 Konekti.toc = function(tree, h, style, onclick, open){
-	return Konekti.build(Konekti.tocConfig(tree, h, style, onclick, open))
+	return Konekti.build(Konekti.plugin['toc'].setup(tree, h, style, onclick, open))
 }
