90297aca2b3c0bb056d68825a6d1bec386d6c734
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-09-08 23:30:39 -0500

    Konekti version 2.1

--- src/btn.js
+++ src/btn.js
@@ -8,60 +8,67 @@ class BtnPlugIn extends PlugIn{
 	 * @param config Instance configuration
 	 */
 	client( config ){ return new Btn(config) }
+
+
 }
 
 /** A Button manager */
 class Btn extends Client{
-	/** 
-	 * Creates a Button Manager
-	 * @param config Configuration of the button
+	/**
+	 * Creates a button configuration object
+	 * @param id Id of the header/Configuration of the header
+	 * @param icon Icon for the header
+	 * @param caption Caption of the header
+	 * @param onclick Information of the method that will be executed when the button is pressed
+	 * @param style Style of the header
+	 * @param title Message that will be shown when mouse is over the button
+	 * @param parent Parent component
 	 */
-	constructor(config){ super(config) }
+	 setup(id, icon, caption, onclick, style, title, parent='KonektiMain'){
+		var config = id
+		if(typeof id == 'string')
+			config = {'id':id, 'style':style, 'run':onclick, 'title':title, 'parent':parent, 'children':[Konekti.plugin['item'].setup(id+'Item', icon, caption, id)]}
+		config.plugin = 'btn'
+		return config
+	}
+
+	/**
+	 * Creates a button configuration object
+	 * @param id Id of the header/Configuration of the header
+	 * @param icon Icon for the header
+	 * @param caption Caption of the header
+	 * @param onclick Information of the method that will be executed when the button is pressed
+	 * @param style Style of the header
+	 * @param title Message that will be shown when mouse is over the button
+	 * @param parent Parent component
+	 */
+	constructor(id, icon, caption, onclick, style, title, parent='KonektiMain'){ super(...arguments) }
 
 	/**
 	 * Associated html code
 	 * @param config Client configuration
 	 */
-	html( config ){ 
-		var title = config.title || ''
-		var style = config.style || ''
-		var run = config.run || ''
+	html(){ 
+		var title = this.config.title || ''
+		var style = this.config.style || ''
+		var run = this.config.run || ''
 		if( typeof run !== 'string' ){
-			var client = config.run.client
-			config.run.method = config.run.method || this.id
-			run = ((client!==undefined && client!==null && client!=='')?'Konekti.client["'+client+'"].':'')+config.run.method+'("'+this.id+'")'
+			var client = this.config.run.client
+			this.config.run.method = this.config.run.method || this.id
+			run = ((client!==undefined && client!==null && client!=='')?'Konekti.client["'+client+'"].':'')+this.config.run.method+'("'+this.id+'")'
 		} 
-		config.config = " title='"+title+"' class='w3-button "+config.style+"' onclick='"+run+"'"
-		return super.html(config)
+		this.config.config = " title='"+title+"' class='w3-button "+style+"' onclick='"+run+"'"
+		return "<div id='" + this.id + "' "+ this.config.config + "></div>"
 	}
 	
 	/**
 	 * Sets a component's attribute to the given value 
 	 * @param config Item configuration
 	 */
-	 update(config){
-		Konekti.client[this.id+'Item'].update(config)
-		if( config.title !== undefined ) this.vc().title = config.title
+	 update(icon, caption, title){
+		Konekti.client[this.id+'Item'].update(icon, caption)
+		if( title !== undefined ) this.vc().title = title
 	}
-}
-
-/** Creates and registers the button plugin */
-if( Konekti.btn === undefined ) new BtnPlugIn()
-
-/**
- * Creates a button configuration object
- * @method
- * btnConfig
- * @param id Id of the header/Configuration of the header
- * @param icon Icon for the header
- * @param caption Caption of the header
- * @param onclick Information of the method that will be executed when the button is pressed
- * @param style Style of the header
- * @param title Message that will be shown when mouse is over the button
- * @param parent Parent component
- */
-Konekti.btnConfig = function(id, icon, caption, onclick, style, title, parent='KonektiMain'){
-	return {'plugin':'btn', 'id':id, 'style':style, 'run':onclick, 'title':title, 'parent':parent, 'children':[{'plugin':'item', 'id':id+'Item', 'icon':icon, 'caption':caption, 'parent':id}]}
 }
 
 /**
@@ -76,5 +83,5 @@ Konekti.btnConfig = function(id, icon, caption, onclick, style, title, parent='K
  * @param title Message that will be shown when mouse is over the button
  */
 Konekti.btn = function(id, icon, caption, onclick, style, title){
-	return Konekti.build(Konekti.btnConfig(id, icon, caption, onclick, style, title))
+	return new Btn(id, icon, caption, onclick, style, title)
 }
