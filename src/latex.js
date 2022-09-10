4f6179c9fb7faff61f1ebb4d8c9554f1e7deef5d
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-09-09 22:54:08 -0500

    Konekti version 2.1

--- src/latex.js
+++ src/latex.js
@@ -3,37 +3,39 @@ if(typeof window.MathJax=='undefined'){
 	Konekti.resource.JS('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js')
 }
 
-/** Konekti Plugin for latex (MathJax) components */
-class LatexPlugIn extends PlugIn{
-	/** Creates a Plugin for latex components */
-	constructor(){ super('latex') }
-
-	/**
-	 * Creates a client for the plugin
-	 * @param config Instance configuration
-	 */
-	client( config ){ return new Latex(config) }
-
-}
-
 /** Latex editor */
 class Latex extends Editor{
 	/**
-	 * Creates a latex editor
-	 * @param config Latex component configuration
+	 * Creates a Latex config object 
+	 * @param id Id of the latex component
+	 * @param width Width of the split component
+	 * @param height Height of the split component
+	 * @param tex Latex code
+	 * @param parent Parent component
 	 */
-	constructor(config){
-		super(config) 
+	setup(id, width, height, tex, parent='KonektiMain'){
+		return {"plugin":"latex", "id":id, "initial":tex, 'width':width, 'height':height, 'parent':parent}
+	}
+
+	/**
+	 * Creates a Latex config object 
+	 * @param id Id of the latex component
+	 * @param width Width of the split component
+	 * @param height Height of the split component
+	 * @param tex Latex code
+	 * @param parent Parent component
+	 */
+	constructor(id, width, height, tex, parent='KonektiMain'){
+		super(...arguments) 
 		this.gui = this.vc()
-		this.initial = config.initial || ''
+		this.initial = this.config.initial || ''
 		this.setText(this.initial)
 	}
 
 	/**
 	 * Associated html code
-	 * @param config Client configuration
 	 */
-	html( config ){ return "<div id='"+this.id+"' style='padding:8px;overflow:auto;'></div>" }
+	html(){ return "<div id='"+this.id+"' style='padding:8px;overflow:auto;'></div>" }
 
 	/**
 	 * Gets current latex code in the component
@@ -66,23 +68,6 @@ class Latex extends Editor{
 	}
 }
 
-/** Latex class */
-if(Konekti.latex===undefined) new LatexPlugIn()
-
-/**
- * Creates a Latex config object 
- * @method
- * latexConfig
- * @param id Id of the latex component
- * @param width Width of the split component
- * @param height Height of the split component
- * @param tex Latex code
- * @param parent Parent component
- */
-Konekti.latexConfig = function(id, width, height, tex, parent='KonektiMain'){
-	return {"plugin":"latex", "id":id, "initial":tex, 'width':width, 'height':height, 'parent':parent}
-}
-
 /**
  * Associates/Adds a latex component
  * @method
@@ -93,7 +78,4 @@ Konekti.latexConfig = function(id, width, height, tex, parent='KonektiMain'){
  * @param tex Latex code
  * @param parent Parent component
  */
-Konekti.latex = function(id, width, height, tex){
-	return Konekti.build(Konekti.latexConfig(id, width, height, tex))
-}
-
+Konekti.latex = function(id, width, height, tex){ return new Latex(id, width, height, tex) }
