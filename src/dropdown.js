4cae0849e48b4683714edfa89bfdb748f1591b09
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-09-10 21:35:19 -0500

    Konekti version 2.1

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,48 +1,55 @@
-uses('btn')
+uses('https://jgomezpe.github.io/konekti/src/btn.js')
 
 /** Konekti Plugin for DropDown components */
-class DropDownPlugIn extends PlugIn{
-	/** Creates a Plugin for Dropdown components */
-	constructor(){ super('dropdown') }    
+class DropDown extends Btn{
+	/**
+	 * Creates a dropdown configuration object
+	 * @param id Id of the dropdown
+	 * @param icon Icon of the dropdown
+	 * @param caption Caption of the dropdown
+	 * @param style Style of the dropdown
+	 * @param title Message that will be shown when mouse is over the dropdown
+	 * @param content_type Type of content: 'free' or 'list'
+	 * @param content Content
+	 * @param parent Parent component
+	 */
+	 setup(id, icon, caption, style, title, content_type, content, parent='KonektiMain'){
+		if( content_type === 'list' ){
+			var options = content.options
+			var onclick = content.onclick
+			for( var i=0; i<options.length; i++ )
+			if( typeof options[i] == 'string' ) options[i] = {'plugin':'btn', 'setup':[options[i], "", options[i], onclick, 'w3-bar-item', options[i]]}
+			else options[i] = {'plugin':'btn', 'setup':[options[i].id, options[i].icon, options[i].caption, onclick, 'w3-bar-item', options[i].title]}
+			content = options
+		}	
+
+		var config= super.setup(id, icon, caption, {"method":"drop", "client":id}, style, title, parent)
+		
+		if(!Array.isArray(content) ) content = [content]
+		var drop = {'plugin':'container', 'setup':[id+'Drop', '', '', "class='w3-dropdown-content w3-bar-block' style='margin-left:-16px;margin-top:6px'", content, id]}
+		config.children.push(drop)
+		config.plugin = 'dropdown'	
+		return config
+	}
 
 	/**
-	 * Gets a client for a Dropdown component
-	 * @param config Dropdown configuration
-	 */  
-	client( config ){ 
-		var client = new Btn(config)
-		client.drop = function(){
-			var x = this.vc('Drop')
-			if (x.className.indexOf("w3-show") == -1) x.className += " w3-show"
-			else x.className = x.className.replace(" w3-show", "")	
-		} 
-		return client 
-	}    
-}
+	 * Creates a dropdown configuration object
+	 * @param id Id of the dropdown
+	 * @param icon Icon of the dropdown
+	 * @param caption Caption of the dropdown
+	 * @param style Style of the dropdown
+	 * @param title Message that will be shown when mouse is over the dropdown
+	 * @param content_type Type of content: 'free' or 'list'
+	 * @param content Content
+	 * @param parent Parent component
+	 */
+	constructor(id, icon, caption, style, title, content_type, content, parent='KonektiMain'){ super(...arguments) }    
 
-/** DropDown class */
-if(Konekti.dropdown === undefined) new DropDownPlugIn()
-
-/**
- * Creates a dropdown configuration object
- * @method
- * dropdownConfig
- * @param id Id of the dropdown
- * @param icon Icon of the dropdown
- * @param caption Caption of the dropdown
- * @param style Style of the dropdown
- * @param title Message that will be shown when mouse is over the dropdown
- * @param content Content
- * @param parent Parent component
- */
-Konekti.dropdownConfig = function(id, icon, caption, style, title, content, parent='KonektiMain'){
-	var btn = Konekti.btnConfig(id, icon, caption, {"method":"drop", "client":id}, style, title, parent)
-	var drop = Konekti.divConfig(id+'Drop', '', '', "class='w3-dropdown-content w3-bar-block' style='margin-left:-16px;margin-top:6px'", '', id)
-	if(Array.isArray(content) ) drop.children = content
-	else drop.children = [content]
-	btn.children.push(drop)
-	btn.plugin = 'dropdown'
-	return btn
+	drop(){
+		var x = this.vc('Drop')
+		if (x.className.indexOf("w3-show") == -1) x.className += " w3-show"
+		else x.className = x.className.replace(" w3-show", "")
+	}
 }
 
 /**
@@ -54,44 +61,10 @@ Konekti.dropdownConfig = function(id, icon, caption, style, title, content, pare
  * @param caption Caption of the dropdown
  * @param style Style of the dropdown
  * @param title Message that will be shown when mouse is over the dropdown
+ * @param content_type Type of content: 'free' or 'list'
  * @param content Content
  * @param parent Parent component
  */
-Konekti.dropdown = function(id, icon, caption, style, title, content){
-	return Konekti.build(Konekti.dropdownConfig(id, icon, caption, style, title, content)) 
-}
-
-/**
- * dropdownList configuration object
- * @method
- * dropdown
- * @param id Id of the dropdown
- * @param icon Icon of the dropdown
- * @param caption Caption of the dropdown
- * @param style Style of the dropdown
- * @param title Message that will be shown when mouse is over the dropdown
- * @param options List of options
- * @param parent Parent component
- */
- Konekti.dropdownListConfig = function(id, icon, caption, style, title, options, onclick, parent='KonektiMain'){
-	for( var i=0; i<options.length; i++ )
-		if( typeof options[i] == 'string' ) options[i] = Konekti.btnConfig(options[i], "", options[i], onclick, 'w3-bar-item', options[i])
-		else options[i] = Konekti.btnConfig(options[i].id, options[i].icon, options[i].caption, onclick, 'w3-bar-item', options[i].title)
-	return Konekti.dropdownConfig(id, icon, caption, style, title, options, parent)
-}
-
-/**
- * Associates/Adds a dropdown with an option list
- * @method
- * dropdown
- * @param id Id of the dropdown
- * @param icon Icon of the dropdown
- * @param caption Caption of the dropdown
- * @param style Style of the dropdown
- * @param title Message that will be shown when mouse is over the dropdown
- * @param options List of options
- * @param parent Parent component
- */
- Konekti.dropdownList = function(id, icon, caption, style, title, options, onclick){
-	return Konekti.build(Konekti.dropdownListConfig(id, icon, caption, style, title, options, onclick))
+Konekti.dropdown = function(id, icon, caption, style, title, content_type, content, parent='KonektiMain'){
+	return new DropDown(id, icon, caption, style, title, content_type, content, parent)
 }
