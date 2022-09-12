3fa9fb3deeacf7e0141be42a090cc6826e2817ec
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-09-12 14:54:35 -0500

    Konekti version 2.1

--- src/tab.js
+++ src/tab.js
@@ -58,21 +58,27 @@ class Tab extends Container{
 	 * @param page Tab to display
 	 */
 	open(page) {
-		page = page.substring(0,page.length-3)
-		if(this.current===undefined) this.current=page
-		var btn
-		if(this.current!==page){
-			btn = Konekti.vc(this.current+'Btn')
-			btn.className = btn.className.replace("w3-grey", "w3-light-grey")
+		var x = this
+		function check(){
+			if( x.children !== undefined && x.children !== null && x.children.length>0 ){
+				page = page.substring(0,page.length-3)
+				if(x.current===undefined) x.current=page
+						var btn
+				if(x.current!==page){
+					btn = Konekti.vc(x.current+'Btn')
+					btn.className = btn.className.replace("w3-grey", "w3-light-grey")
+				}
+				x.current=page
+				var tabs = x.children[1].children
+				btn = Konekti.vc(x.current+'Btn')
+				btn.className = btn.className.replace("w3-light-grey", "w3-grey")
+				for( var c in tabs ) tabs[c].vc().style.display = "none"  
+				Konekti.vc(page).style.display = "initial"
+				var ta = Konekti.vc(page).getElementsByTagName('textarea')
+				for( var k=0; k<ta.length; k++ ) ta[k].focus()
+			}else setTimeout(check, 100)
 		}
-		this.current=page
-		var tabs = this.children[1].children
-		btn = Konekti.vc(this.current+'Btn')
-		btn.className = btn.className.replace("w3-light-grey", "w3-grey")
-		for( var c in tabs ) tabs[c].vc().style.display = "none"  
-		Konekti.vc(page).style.display = "initial"
-		var ta = Konekti.vc(page).getElementsByTagName('textarea')
-		for( var k=0; k<ta.length; k++ ) ta[k].focus()
+		check()
 	}
 }
 
