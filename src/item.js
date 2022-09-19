e94fb54f1fe7a02d751862e6417e0f6e21b2c765
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-09-19 13:05:03 -0500

    Adds image and fa stack as icons

--- src/item.js
+++ src/item.js
@@ -23,16 +23,41 @@ class Item extends Client{
 	/**
 	 * Associated html code
 	 */
-	html(){ return "<i id='"+this.id+"' class='fa "+this.config.icon+"'> "+this.config.caption+"</i>" }   
+	html(){ return '<div id="'+this.id+'" style:"vertical-align:middle;" >' + this.inner_html() +'</div>' } 
+
+	inner_html(){
+		var el = Konekti.vc(this.parent)
+		var style = window.getComputedStyle(el, null).getPropertyValue('font-size')
+		var fontSize = parseFloat(style) * 1.3
+		var code = ''
+		if(typeof this.config.icon == 'string') this.config.icon = {'type':'fa', 'src':this.config.icon} // Backward compatibility
+		switch(this.config.icon.type){
+			case 'fa':
+				if(typeof this.config.icon.src == 'string') 
+					code = '<i id="'+this.id+'Icon" class="fa '+this.config.icon.src+'"></i>'
+				else{					
+					code = '<span class="fa-stack">\n'
+					code += '<i class="fa ' + this.config.icon.src[0] + ' fa-stack-2x"></i>\n'
+					code += '<i class="fa ' + this.config.icon.src[1] + ' fa-stack-1x"></i>\n'
+				  	code += '</span>\n'
+				}
+			break;
+			case 'img':
+				code = '<div style="float:left;width:' + fontSize +'px;height:100%;"><img src="' + this.config.icon.src + '" style="width:100%;height:100%"></div>' 
+			break;
+		}
+		return code + this.config.caption
+	}
 	
 	/**
 	 * Sets a component's attribute to the given value 
 	 * @param config Item configuration
 	 */
-	update(icon='', caption=''){
+	update(icon, caption){
 		var c = this.vc()
-		c.innerHTML = " " + caption
-		c.className = 'fa ' + icon
+		this.config.icon = icon
+		this.config.caption = caption
+		c.innerHTML = this.inner_html()
 	}
 }
 
