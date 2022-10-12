a7d7ea87d1fc5bfb5323a21ec6a856c63d1b3e75
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2022-10-11 21:05:54 -0500

    Adds inputlist component

---
+++ src/inputlist.js
@@ -0,0 +1,95 @@
+/** Konekti plugin for input list elements */
+class InputListPlugin extends PlugIn{
+	constructor(){ super('inputlist') }
+
+	/**
+	 * Creates an input-list configuration object
+	 * @param parent Parent component
+	 * @param id Id of the input-list
+	 * @param options List options
+	 * @param onselect Method that will be executed when an option of the list is selected
+	 * @param onenter Method that will be executed when the enter key is pressed
+	 * @param config Style of the input-list
+	 */
+	setup(parent, id, options, onselect='', onenter='', config={}){
+		config.tag = 'input'
+		config.class = (config.class||"") + " w3-input "
+		config.placeholder = '&#xF002; ' + (config.placeholder || '')
+		config.name = id
+		config.list = id+'List'
+		var c = super.setup(parent, id, '', config)
+		c.options = options
+		c.onenter = onenter
+		c.onselect = onselect
+		return c
+	}
+
+	client(config){ return new InputList(config) }
+}
+
+/** Registers the inputlist plugin in Konekti */
+new InputListPlugin()
+
+/** An InputList manager */
+class InputList extends Client{
+
+	/**
+	 * Creates an inputlist client
+	 */
+	constructor(config){ 
+		super(config)
+		var x = this
+		Konekti.raw('body', this.id+'List', '', {'tag':'datalist'})
+		x.vc().onchange = function(){
+			var value = x.vc().value
+			var i=0
+			while(i<x.options.length && x.options[i].caption!=value) i++
+			if(i<x.options.length) eval(Konekti.dom.onclick(x.options[i].id,x.onselect))
+			else eval(Konekti.dom.onclick(value,x.onenter))
+			x.vc().value = ''
+		}
+
+		this.set(this.options)
+	}
+
+	set(list){
+		console.log(list)
+		var x = this
+		x.options = list
+		var tout
+		function check(){
+			if(x.vc('List')!==undefined && x.vc('List')!==null){
+				clearTimeout(tout)
+				var options = ''
+				for(var i=0; i<list.length; i++){
+					options += '<option data-value="'+list[i].id+'">'+list[i].caption+'</option>\n'
+				}
+				x.vc('List').innerHTML = options
+			}else tout = setTimeout(check, Konekti.TIMER)
+		}
+		check()
+	}
+}
+
+/**
+ * Associates/adds a header
+ * @method
+ * btn
+ * @param parent Parent component
+ * @param id Id of the inputlist
+ * @param options List of options
+ * @param onselect Method that will be executed when an option of the list is selected
+ * @param onenter Method that will be executed when the enter key is pressed
+ * @param config Style of the inputlist
+ * @param callback Function called when the inputlist is ready
+ */
+Konekti.inputlist = function(parent, id, options, onselect, onenter, config, callback){ 
+	var args = []
+	for(var i=0; i<arguments.length; i++) args[i] = arguments[i]
+	if(args.length==2) args[2] = []
+	if(args.length==3) args[3] = ''
+	if(args.length==4) args[4] = ''
+	if(args.length==5) args[5] = {}
+	if(args.length==6) args[6] = function(){}
+	Konekti.add('inputlist', ...args)
+}
