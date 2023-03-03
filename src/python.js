325c7548139230d850be685b161a4f80d40dbe2c
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2023-03-03 12:27:49 -0500

    Update python component

--- src/python.js
+++ src/python.js
@@ -114,7 +114,7 @@ class Python extends Client{
             Konekti.client[x.id+'run'].update('fa fa-stop', '', '')
             x.terminal.init(x.greetings)
             var args = []
-            for(var i=0; i<x.tabs.length; i++) args[i] = {'name':x.tabs[i], 'code':Konekti.client[x.id+x.tabs[i].name].getText()})
+            for(var i=0; i<x.tabs.length; i++) args[i] = {'name':x.tabs[i], 'code':Konekti.client[x.id+x.tabs[i].name].getText()}
             x.process.run(...args)
             x.running = true
             x.vc('console').focus()
