7caa320713449aef88758a888689cd5db21e6374
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2023-04-11 08:47:23 -0500

    Update to version 4.0

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,63 +1,3 @@
-Konekti.dom.css(
-	`.w4-main{
-		height:100%;
-		margin-left:200px;
-		transition:margin-left .4s;
-		position:fixed!important;
-		overflow:auto;
-	}
-	  
-	.w4-sidebar{
-		height:100%;
-		width:200px;
-		background-color:#fff;
-		position:fixed!important;
-		z-index:40;
-		overflow:auto;
-	}
-	  
-	.w4-sidebtn{
-		height:100%;
-		width:20px;
-		background-color:#fff;
-		opacity:0.4;
-		position:fixed;
-		cursor:pointer;
-	}
-	  
-	@media (min-width:` + (Konekti.MEDIUM_SIZE+1) + `px){
-		.w4-main{
-			margin-left:200px!important;
-			margin-right:0px!important;
-		}
-		
-		.w4-sidebtn{
-		   display:none;
-		}
-	   
-		.w4-sidebar{
-			display:block;
-		}
-	}
-
-	  
-	@media (max-width:` + Konekti.MEDIUM_SIZE + `px){
-		.w4-sidebar{
-			display:none;
-		}
-	   
-		.w4-sidebtn{
-			display:block;
-		}
-	   
-		.w4-main{
-			margin-left:20px!important;
-			margin-right:0px!important;
-		}
-	}`
-)
-
-
 /** Konekti plugin for sidebarb elements */
 class SideBarPlugin extends PlugIn{
 	constructor(){ super('sidebar') }
@@ -72,14 +12,21 @@ class SideBarPlugin extends PlugIn{
 	 */
 	 setup(parent, id, side, main, config={}){
 		var client = 'Konekti.client["'+id+'"].'
+		var side_width = config['side-width'] || 200
+		delete config['side-width']
 		var side = {'plugin':'raw', 'setup':[id+'Bar', [
-			{'plugin':'btn', 'setup':[id+'Close', 'fa fa-times', '', client+'close()', {'class':' w3-large w3-hide-large w3-right-align '}]},
+			{'plugin':'btn', 'setup':[id+'Close', 'fa fa-times', '', client+'close()', {'class':' w3-large w3-right-align '}]},
 			side
-		], {'class':'w4-sidebar w3-collapse w3-card w3-animate-left'}]}
+		], {'class':'w3-card w3-animate-left ', 'height':'100%', 'width':side_width+'px',
+			'style':{'background-color':'#fff',	'position':'fixed!important', 'display':'none', 'z-index':'40', 'overflow':'auto'}}]}
 		config.layout='row'
-		var expand = {'plugin':'raw', 'setup':[id+'Resize', '&#9776;', {'width':'20px','onclick':client+"open()", 'class':' w3-large w4-sidebtn '}]}
-		var main = {'plugin':'raw', 'setup':[id+'Main', main, {'class':' w4-main ', 'style':'margin-left:200px;height:100%;'}]}
-		return super.setup(parent, id, [expand,side,main], config)
+		var expand = {'plugin':'raw', 'setup':[id+'Resize', '&#9776;', {'width':'20px','onclick':client+"open()", 'class':' w3-large ',
+			'style':{'background-color':'#fff',	'position':'fixed', 'opacity':0.4, 'cursor':'pointer', 'overflow':'auto'}}]}
+		var main = {'plugin':'raw', 'setup':[id+'Main', main, {'height':'100%', 'style':{'transition':'margin-left .4s',	
+					'position':'fixed!important', 'overflow':'auto'}}]}
+		var c = super.setup(parent, id, [expand,side,main], config)
+		c.side_width = side_width
+		return c
 	}
 	
 	client(config){ return new SideBar(config) }
@@ -114,7 +61,7 @@ class SideBar extends Client{
 		var x = this
 		x.vc('Resize').style.display = 'block' 
 		x.vc('Bar').style.display = 'none' 
-		x.update(x.width-20, x.height)
+		x.update(x.width, x.height)
 	}
 
     /**
@@ -125,11 +72,22 @@ class SideBar extends Client{
     update( width, height ){
 		var x = this
 		if(width>Konekti.MEDIUM_SIZE){
+			x.vc('Resize').style.display = 'none' 
+			x.vc('Bar').style.display = 'block' 
+			x.vc('Close').style.display = 'none'
 			x.children[0].resize(0,height)
-			x.children[1].resize(200,height)
-			x.children[2].resize(width-200,height)
+			x.children[1].resize(x.side_width,height)
+			x.children[2].vc().style.marginLeft=x.side_width+'px'
+			x.children[2].resize(width-x.side_width,height)
 		}else{
-			x.children[2].resize(width,height)
+			x.vc('Close').style.display = 'block'
+			if(x.vc('Bar').style.display=='block'){
+				x.children[2].vc().style.marginLeft='0px'
+				x.children[2].resize(width,height)
+			}else{
+				x.children[2].vc().style.marginLeft='20px'
+				x.children[2].resize(width-20,height)
+			}
 		}
 	}
 
