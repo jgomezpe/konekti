8855159cde0cec97731ec5f481df110a838093d0
Jonatan Gomez Perdomo <jgomezpe@unal.edu.co>
2023-06-05 19:57:49 -0500

    Using $ as inline delimiter

--- src/latex.js
+++ src/latex.js
@@ -1,10 +1,8 @@
 if(typeof window.MathJax=='undefined'){
-	var script = document.createElement('script')
-	script.setAttribute('type', 'text/x-mathjax-config');
-	script.innerHTML = "MathJax = { tex:{ inlineMath: [['$', '$'], ['\\(', '\\)']], processEscapes: true } }"
-	var b = document.getElementsByTagName('script')[0]
-	b.parentNode.insertBefore(script, b)
-	Konekti.resource.JS('https://polyfill.io/v3/polyfill.min.js?features=es6')
+	window.MathJax = { 
+		tex:{ inlineMath: [['$', '$'], ['\\(', '\\)']] }
+	}
+//	Konekti.resource.JS('https://polyfill.io/v3/polyfill.min.js?features=es6')
 	Konekti.resource.JS('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js')
 }
 
